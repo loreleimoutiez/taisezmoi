2e2791a8cc0601aade8d6831e54fa3bcf85c8908
Lorelei <loreleimoutiez@gmail.com>
2024-07-17 12:16:56 +0200

    add dist

--- src/frontend/js/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from '@/App.vue'
-import '@/index.css'
-import router from '@/frontend/router'
-import 'quill/dist/quill.snow.css'
-
-createApp(App).use(router).mount('#app')
