2aff645cf7a9d42936facf44a976f7505359fce8
Lorelei <loreleimoutiez@gmail.com>
2025-03-04 18:53:02 +0100

    add dist

--- src/frontend/frontend-vue/Components/LatestNews.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex justify-center px-6 bg-fuchsia py-2.5 sm:px-3.5">
-    <p class="text-sm leading-7 text-white md:text-base">
-      <strong class="font-semibold animate-pulse">À la recherche d'opportunités au Québec</strong>
-    </p>
-  </div>
-</template>
