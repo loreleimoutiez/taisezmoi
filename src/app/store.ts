d1cc2b9d193f1352f090d496162c16b9fec9ca98
Lorelei <loreleimoutiez@gmail.com>
2025-07-03 17:25:40 +0200

    add dist

--- src/app/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from '../features/theme/theme-slice';
-
-export default configureStore({
-    reducer: {
-        theme: themeReducer,
-    },
-});
